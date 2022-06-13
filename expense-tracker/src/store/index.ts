12d3a5e13b735e3cae52aa8770830a87d056a565
kadirkara22 <kadirkar2204@gmail.com>
2022-06-13 15:45:08 +0300

    harcama raporları eklendi

--- expense-tracker/src/store/index.ts
+++ expense-tracker/src/store/index.ts
@@ -3,17 +3,19 @@ import { combineReducers } from "redux";
 import { UserState } from "../types/user";
 import userReducer from "./reducers/userReducer";
 import categoryReducer from "./reducers/categoryReducer";
+import { RecordState } from "../types/record";
+import recordReducer from "./reducers/recordReducer";
 
 export interface AppState {
   user: UserState;
   categories: CategoryState;
-  //records: any;
+  records: RecordState;
 }
 
 const rootReducer = combineReducers<AppState>({
   user: userReducer,
   categories: categoryReducer,
-  //records: () => {},
+  records: recordReducer,
 });
 
 export default rootReducer;
